ac1e4966f3f0a4476bb41b52816def6738598502
PresentPal <business.harryjordan@gmail.com>
2025-03-05 21:10:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ import "./js/firebase.js";      // Handles Firestore data
 import "./js/navigation.js";    // Handles navigation and UI updates
 import "./js/subscriptions.js"; // Handles Stripe payments
 import "./js/pwa.js";           // Handles Progressive Web App features
+
+// Initialize Firebase Auth
+const auth = getAuth();
+
+// Export auth for other files to use
+export { auth };
