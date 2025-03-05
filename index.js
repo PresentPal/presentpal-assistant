be8b9c5784fc2e750b2ef0f624fcca25563cf83c
PresentPal <business.harryjordan@gmail.com>
2025-03-05 18:47:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "./js/auth.js";          // Handles login, signup, and logout
-import "./js/database.js";      // Handles Firestore data
+import "./js/firebase.js";      // Handles Firestore data
 import "./js/navigation.js";    // Handles navigation and UI updates
 import "./js/subscriptions.js"; // Handles Stripe payments
 import "./js/pwa.js";           // Handles Progressive Web App features
