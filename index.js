78d12087fff2454edeca4ca546313393829eeb11
Harry | PresentPal <business.harryjordan@gmail.com>
2025-03-19 21:24:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,29 +13,3 @@ const auth = getAuth();
 
 // Export auth for other files to use
 export { auth };
-
-// Function to show the correct chatbot based on the user's subscription and package
-function showChatbotBasedOnSubscription(subscription, packageType) {
-    // Get the containers for each chatbot
-    const chatbotFree = document.getElementById("chatbotFree");
-    const chatbotPlus = document.getElementById("chatbotPlus");
-    const chatbotPremium = document.getElementById("chatbotPremium");
-
-    // Hide all chatbots initially
-    chatbotFree.style.display = "none";
-    chatbotPlus.style.display = "none";
-    chatbotPremium.style.display = "none";
-
-    // Logic to show the correct chatbot based on subscription and package
-    if (subscription === "freeUser") {
-        // For free users, show PresentPal chatbot
-        chatbotFree.style.display = "block";
-    } else if (subscription === "subscribedUser") {
-        // For subscribed users, check the package
-        if (packageType === "PresentPal+") {
-            chatbotPlus.style.display = "block";
-        } else if (packageType === "PresentPal Premium") {
-            chatbotPremium.style.display = "block";
-        }
-    }
-}
