6861edf47ad9aaef87df75f50002861b02112fef
Harry | PresentPal <business.harryjordan@gmail.com>
2025-03-21 10:52:56 +0000

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("homeButton").addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+
+    document.getElementById("accountButton").addEventListener("click", function () {
+        window.location.href = "account.html";
+    });
+
+    document.getElementById("dashboardButton").addEventListener("click", function () {
+        window.location.href = "dashboard.html";
+    });
+
+    document.getElementById("upgradeButton").addEventListener("click", function () {
+        window.location.href = "upgrade.html";
+    });
+});
 
