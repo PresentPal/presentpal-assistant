688fbe84d1b1d5cc755a3bbad1c478ad14ca3194
Harry | PresentPal <business.harryjordan@gmail.com>
2025-03-23 18:39:18 +0000

    Update recipients.js

--- js/recipients.js
+++ js/recipients.js
@@ -68,7 +68,7 @@ row.innerHTML = `
         row.innerHTML = `
           <td>${data.name}</td>
           <td>${data.relationship || ""}</td>
-          <td colspan="2">No occasions found</td>
+          <td colspan="1">No occasions found</td>
           <td>${data.age || ""}</td>
           <td>${data.gender || ""}</td>
           <td>${Array.isArray(data.interests) ? data.interests.join(", ") : data.interests || ""}</td>
