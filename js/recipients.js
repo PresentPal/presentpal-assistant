c28e4962190756ba375ce3a87e39616aeabd0687
Harry | PresentPal <business.harryjordan@gmail.com>
2025-03-23 12:00:20 +0000

    Update recipients.js

--- js/recipients.js
+++ js/recipients.js
@@ -30,6 +30,7 @@ async function loadRecipients() {
 
             row.innerHTML = `
                 <td>${data.name}</td>
+                <td>${data.name}</td>
             `;
 
             row.addEventListener("click", () => openManageRecipientModal(docSnapshot.id, data));
