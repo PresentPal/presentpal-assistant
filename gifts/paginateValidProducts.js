8004d4fe678b428ac37df71d3c7c7909088326db
Harry | PresentPal <business.harryjordan@gmail.com>
2025-03-28 14:58:33 +0000

    Create paginateValidProducts.js

---
+++ gifts/paginateValidProducts.js
@@ -0,0 +1,17 @@
+window.paginateValidProducts function () {
+  const valid = filteredProducts.filter(p =>
+    p.image && p.link && p.image.startsWith("http") && p.link.startsWith("http")
+  );
+
+  paginatedProducts = [];
+  for (let i = 0; i < valid.length; i += itemsPerPage) {
+    const page = valid.slice(i, i + itemsPerPage);
+    if (page.length === itemsPerPage || i + itemsPerPage >= valid.length) {
+      paginatedProducts.push(page);
+    } else {
+      const remaining = valid.length - i;
+      paginatedProducts.push(valid.slice(i)); // final smaller page
+      break;
+    }
+  }
+}
