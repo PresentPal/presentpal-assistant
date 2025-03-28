7a54efc6db47a072ee56ca08c02cbc588168df8a
Harry | PresentPal <business.harryjordan@gmail.com>
2025-03-28 15:16:53 +0000

    Create populate.js

---
+++ gifts/populate.js
@@ -0,0 +1,15 @@
+window.populateCategoryDropdown = function () {
+  const select = document.getElementById("categoryFilter");
+  select.innerHTML = '<option value="">Filter by Category</option>';
+  Object.entries(categoryKeywords).forEach(([main, subs]) => {
+    const optGroup = document.createElement("optgroup");
+    optGroup.label = main;
+    Object.entries(subs).forEach(([sub, keywords]) => {
+      const opt = document.createElement("option");
+      opt.value = keywords[0];
+      opt.textContent = sub;
+      optGroup.appendChild(opt);
+    });
+    select.appendChild(optGroup);
+  });
+}
